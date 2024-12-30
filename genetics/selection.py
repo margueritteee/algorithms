18290078be14e53f6167f1d0e2641b805052ea13
kezrane margueritte <121063816+margueritteee@users.noreply.github.com>
2024-12-30 20:15:44 +0100

    selection.py

--- genetics/selection.py
+++ genetics/selection.py
@@ -1,8 +1,8 @@
 import random
-
+#selecting individuals from a given population based on their fitness scores
 def selection(population, fitness_scores):
-    tournament_size = 3
-    selected = []
+    tournament_size = 3 #the number of individuals randomly chosen to compete in each tournament
+    selected = [] #will store the individuals chosen
     for _ in range(len(population)):
         tournament = random.sample(list(enumerate(population)), tournament_size)
         winner = max(tournament, key=lambda x: fitness_scores[x[0]])
