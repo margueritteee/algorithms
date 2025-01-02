ef5225d9ec5c490587cbe1d3804b751a44025b39
kezrane margueritte <121063816+margueritteee@users.noreply.github.com>
2025-01-02 17:34:38 +0100

    initialize.py

--- genetics/initialize.py
+++ genetics/initialize.py
@@ -1,4 +1,11 @@
-import numpy as np
+import random
 
-def initialize_population(pop_size, num_items):
-    return [np.random.randint(2, size=num_items) for _ in range(pop_size)]
+def init_population(weights, pop_size): #function takes 2 params (weights:list of item weights / pop_size:how many solutions to create)
+    return [[random.randint(0, 1) for _ in range(len(weights))] for _ in range(pop_size)] #loop for pop_size (create gdash mn solutions) / loop deuxieme len(weights)times (create one solution) w ha tkon random bin 0,1
+
+#example :For weights=[10,20,30] and pop_size=5 [
+    #[1, 0, 1],  
+    #[0, 1, 0],
+    #[1, 1, 0], 
+    #[0, 0, 1],  
+    #[1, 0, 0]] lazm tfotsh 60 we verfying the total weight in fitness fun
