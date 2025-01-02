39ec59da7326e7dabae717e25a8c3d38209d74ba
kezrane margueritte <121063816+margueritteee@users.noreply.github.com>
2025-01-02 17:49:35 +0100

    selection.py

--- genetics/selection.py
+++ genetics/selection.py
@@ -1,10 +1,10 @@
 import random
-#selecting individuals from a given population based on their fitness scores
-def selection(population, fitness_scores):
-    tournament_size = 3 #the number of individuals randomly chosen to compete in each tournament
-    selected = [] #will store the individuals chosen
-    for _ in range(len(population)):
-        tournament = random.sample(list(enumerate(population)), tournament_size)
-        winner = max(tournament, key=lambda x: fitness_scores[x[0]])
-        selected.append(winner[1])
-    return selected
+from fitness import fitness
+
+def selection(population, weights, profits, capacity):
+    scores = [fitness(chrom, weights, profits, capacity) for chrom in population] #scores is a list fiha fitness values ntae kol chromosome
+    total = sum(scores) #total fitness ntae population
+    if total == 0:
+        return random.sample(population, 2) #ida kano gae fitness ntaehm zero nkhyro 2 chromo randomly
+    probs = [s / total for s in scores] #nhsbo probab ntae kol chromo ( fitness ntae kol chromo ela total fitness)
+    return random.choices(population, weights=probs, k=2) #function selects parents with higher fitness scores more likely to be chosen
